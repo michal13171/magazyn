38b7132feaae54afb149875e0a3f1de373cf2c98
Michał Kukuła <michal13171@wp.pl>
2018-10-01 12:02:30 +0200

    getAll records

--- view/resource/viewProduct/index.twig.php
+++ view/resource/viewProduct/index.twig.php
@@ -6,11 +6,13 @@ include VIEW . 'inc/header.php';
         <li><a href="{{ item.href }}">{{ item.caption }}</a></li>
         {% endfor %}
     </ul>
-
     <h1>My folder produkt</h1>
-    {% for name in navigation %}
-    <li>>{{ navigation.name }}</li>
-    {% endfor %}
+
+    <ul id="navigation">
+        {% for product in products %}
+        <li><a href="{{ produkt.id }}">{{ product.name }}</a></li>
+        {% endfor %}
+    </ul>
 <?php
 
 include VIEW . 'inc/footer.php';
